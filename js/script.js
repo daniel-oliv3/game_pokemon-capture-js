b440b96adca9cdf104b817e89837edd3781cd367
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-14 23:52:28 -0300

    Pokeball

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /*Criando os arbustos*/
 const NUM_BUSHES = 50;
+const NUM_BALLS = 5
 
 function createBushes(){
     for(let i = 0; i < NUM_BUSHES; i++){
@@ -11,9 +12,22 @@ function createBushes(){
     }
 }
 
+/*Criando as pokeball*/
+function createBalls(){
+    for(let i = 0; i < NUM_BALLS; i++){
+        const div = document.createElement('div');
+        div.classList.add('pokeball');
+        div.style.left = Math.random() * 100 + '%';
+        div.style.top = Math.random() * 100 + '%';
+        document.body.appendChild(div);
+    }
+}
+
+
 
 function init(){
     createBushes();
+    createBalls();
 }
 
 init();
